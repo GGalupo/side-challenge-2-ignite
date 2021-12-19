d975b514f86621bd90a49ca7a2136a651453bf4c
Gustavo Galupo <ggalupo2@hotmail.com>
2021-12-19 16:52:09 -0300

    Refactored Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,18 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
+import { IconType } from "react-icons";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  placeholder: string;
+  icon?: IconType;
+}
+
+const Input = ({ name, placeholder, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -31,12 +33,12 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
   return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container $isFilled={isFilled} $isFocused={isFocused}>
       {Icon && <Icon size={20} />}
 
       <input
